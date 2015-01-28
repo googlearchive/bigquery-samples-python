fccc42b565600aaa3b40b196e84eb660873a9379
Johan Euphrosine <proppy@google.com>
2015-01-27 17:10:58 -0800

    dump: make python green

--- dump/loading-data-into-bigquery.py
+++ dump/loading-data-into-bigquery.py
@@ -3,24 +3,24 @@ import json
 
 from apiclient.discovery import build
 from oauth2client.file import Storage
-from oauth2client.client import AccessTokenRefreshError
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.tools import run
-from apiclient.errors import HttpError
 import httplib2
 
 FLOW = OAuth2WebServerFlow(
-client_id='xxxxxxx.apps.googleusercontent.com',
-client_secret='shhhhhhhhhhhh',
-scope='https://www.googleapis.com/auth/bigquery',
-user_agent='my-program-name/1.0')
+    client_id='xxxxxxx.apps.googleusercontent.com',
+    client_secret='shhhhhhhhhhhh',
+    scope='https://www.googleapis.com/auth/bigquery',
+    user_agent='my-program-name/1.0')
+
 
 def loadTable(http, service):
     projectId = raw_input("Choose your project ID: ")
-    vdatasetId = raw_input("Choose a dataset ID: ")
+    datasetId = raw_input("Choose a dataset ID: ")
     tableId = raw_input("Choose a table name to load the data to: ")
 
-    url = "https://www.googleapis.com/upload/bigquery/v2/projects/" + projectId + "/jobs"
+    url = ("https://www.googleapis.com/upload/bigquery/v2/projects/" +
+           projectId + "/jobs")
     newSchemaFile = raw_input("What is your schema? ")
     schema = open(newSchemaFile, 'r')
 
@@ -54,7 +54,8 @@ def loadTable(http, service):
     newresource += ('--xxx--\n')
 
     headers = {'Content-Type': 'multipart/related; boundary=xxx'}
-    resp, content = http.request(url, method="POST", body=newresource, headers=headers)
+    resp, content = http.request(url, method="POST",
+                                 body=newresource, headers=headers)
 
     if resp.status == 200:
         jsonResponse = json.loads(content)
@@ -62,34 +63,37 @@ def loadTable(http, service):
     import time
     while True:
         jobCollection = service.jobs()
-    getJob = jobCollection.get(projectId=projectId, jobId=jobReference).execute()
+    getJob = jobCollection.get(projectId=projectId,
+                               jobId=jobReference).execute()
     currentStatus = getJob['status']['state']
 
     if 'DONE' == currentStatus:
         print "Done Loading!"
-    return
-
+        return
     else:
         print 'Waiting to load...'
     print 'Current status: ' + currentStatus
     print time.ctime()
     time.sleep(10)
 
+
 def main(argv):
     # If the credentials don't exist or are invalid, run the native client
     # auth flow. The Storage object will ensure that if successful the good
     # credentials will get written back to a file.
-    storage = Storage('bigquery2.dat') # Choose a file name to store the credentials.
+    #
+    # Choose a file name to store the credentials.
+    storage = Storage('bigquery2.dat')
     credentials = storage.get()
     if credentials is None or credentials.invalid:
         credentials = run(FLOW, storage)
 
-    # Create an httplib2.Http object to handle our HTTP requests and authorize it
-    # with our good credentials.
+    # Create an httplib2.Http object to handle our HTTP requests
+    # and authorize it with our good credentials.
     http = httplib2.Http()
     http = credentials.authorize(http)
 
-    service = build('bigquery','v2', http=http)
+    service = build('bigquery', 'v2', http=http)
 
     loadTable(http, service)
 
--- dump/querying_data.py
+++ dump/querying_data.py
@@ -1,137 +1,158 @@
+import pprint
+import time
+
+from oauth2client.client import AccessTokenRefreshError
+from apiclient.errors import HttpError
+
+
+def printTableData():
+    pass
+
 # [START synchronous_call]
 # Run a synchronous query, save the results to a table, overwriting the
 # existing data, and print the first page of results.
 # Default timeout is to wait until query finishes.
+
+
 def runSyncQuery(service, projectId, datasetId, timeout=0):
-  try:
-    print 'timeout:%d' % timeout
-    jobCollection = service.jobs()
-    queryData = {'query':'SELECT word,count(word) AS count FROM publicdata:samples.shakespeare GROUP BY word;',
-                 'timeoutMs':timeout}
+    try:
+        print 'timeout:%d' % timeout
+        jobCollection = service.jobs()
+        queryData = {'query': 'SELECT word,count(word) AS count'
+                              'FROM publicdata:samples.shakespeare'
+                              'GROUP BY word;',
+                     'timeoutMs': timeout}
 
-    queryReply = jobCollection.query(projectId=projectId,
-                                     body=queryData).execute()
+        queryReply = jobCollection.query(projectId=projectId,
+                                         body=queryData).execute()
 
-    jobReference=queryReply['jobReference']
+        jobReference = queryReply['jobReference']
 
-    # Timeout exceeded: keep polling until the job is complete.
-    while(not queryReply['jobComplete']):
-      print 'Job not yet complete...'
-      queryReply = jobCollection.getQueryResults(
-                          projectId=jobReference['projectId'],
-                          jobId=jobReference['jobId'],
-                          timeoutMs=timeout).execute()
+        # Timeout exceeded: keep polling until the job is complete.
+        while not queryReply['jobComplete']:
+            print 'Job not yet complete...'
+            queryReply = jobCollection.getQueryResults(
+                projectId=jobReference['projectId'],
+                jobId=jobReference['jobId'],
+                timeoutMs=timeout).execute()
 
-    # If the result has rows, print the rows in the reply.
-    if('rows' in queryReply):
-      print 'has a rows attribute'
-      printTableData(queryReply, 0)
-      currentRow = len(queryReply['rows'])
+        # If the result has rows, print the rows in the reply.
+        if 'rows' in queryReply:
+            print 'has a rows attribute'
+            printTableData(queryReply, 0)
+            currentRow = len(queryReply['rows'])
 
-      # Loop through each page of data
-      while('rows' in queryReply and currentRow < queryReply['totalRows']):
-        queryReply = jobCollection.getQueryResults(
-                          projectId=jobReference['projectId'],
-                          jobId=jobReference['jobId'],
-                          startIndex=currentRow).execute()
-        if('rows' in queryReply):
-          printTableData(queryReply, currentRow)
-          currentRow += len(queryReply['rows'])
+            # Loop through each page of data
+            while ('rows' in queryReply and
+                    currentRow < queryReply['totalRows']):
+                queryReply = jobCollection.getQueryResults(
+                    projectId=jobReference['projectId'],
+                    jobId=jobReference['jobId'],
+                    startIndex=currentRow).execute()
+                if ('rows' in queryReply):
+                    printTableData(queryReply, currentRow)
+                    currentRow += len(queryReply['rows'])
 
-  except AccessTokenRefreshError:
-    print ("The credentials have been revoked or expired, please re-run"
-    "the application to re-authorize")
+    except AccessTokenRefreshError:
+        print ('The credentials have been revoked or expired, please re-run'
+               'the application to re-authorize')
 
-  except HttpError as err:
-    print 'Error in runSyncQuery:', pprint.pprint(err.content)
+    except HttpError as err:
+        print 'Error in runSyncQuery:', pprint.pprint(err.content)
 
-  except Exception as err:
-    print 'Undefined error' % err
+    except Exception as err:
+        print 'Undefined error' % err
 # [END synchronous_call]
+
 
 # [START asynchronous_call]
 def runAsyncQuery(service, projectId):
-  try:
-    jobCollection = service.jobs()
-    queryString = 'SELECT corpus FROM publicdata:samples.shakespeare GROUP BY corpus;'
-    jobData = {
-      'configuration': {
-        'query': {
-          'query': queryString,
+    try:
+        jobCollection = service.jobs()
+        queryString = ('SELECT corpus FROM publicdata:samples.shakespeare'
+                       'GROUP BY corpus;')
+        jobData = {
+            'configuration': {
+                'query': {
+                    'query': queryString,
+                }
+            }
         }
-      }
-    }
 
-    insertResponse = jobCollection.insert(projectId=projectId,
-                                         body=jobData).execute()
+        insertResponse = jobCollection.insert(projectId=projectId,
+                                              body=jobData).execute()
 
-    # Get query results. Results will be available for about 24 hours.
-    currentRow = 0
-    queryReply = jobCollection.getQueryResults(
-                      projectId=projectId,
-                      jobId=insertResponse['jobReference']['jobId'],
-                      startIndex=currentRow).execute()
+        # Get query results. Results will be available for about 24 hours.
+        currentRow = 0
+        queryReply = jobCollection.getQueryResults(
+            projectId=projectId,
+            jobId=insertResponse['jobReference']['jobId'],
+            startIndex=currentRow).execute()
 
-    while(('rows' in queryReply) and currentRow < queryReply['totalRows']):
-      printTableData(queryReply, currentRow)
-      currentRow += len(queryReply['rows'])
-      queryReply = jobCollection.getQueryResults(
+        while(('rows' in queryReply) and currentRow < queryReply['totalRows']):
+            printTableData(queryReply, currentRow)
+            currentRow += len(queryReply['rows'])
+            queryReply = jobCollection.getQueryResults(
+                projectId=projectId,
+                jobId=queryReply['jobReference']['jobId'],
+                startIndex=currentRow).execute()
+
+    except HttpError as err:
+        print 'Error in runAsyncTempTable:', pprint.pprint(err.resp)
+
+    except Exception as err:
+        print 'Undefined error' % err
+# [END asynchronous_call]
+
+
+# [START batched_call]
+def runBatchedCall(service, projectId):
+    try:
+        jobCollection = service.jobs()
+        queryString = ('SELECT corpus FROM publicdata:samples.shakespeare'
+                       'GROUP BY corpus;')
+        jobData = {
+            'configuration': {
+                'query': {
+                    'query': queryString,
+                    'priority': 'BATCH'  # Set priority to BATCH
+                }
+            }
+        }
+
+        insertResponse = jobCollection.insert(projectId=projectId,
+                                              body=jobData).execute()
+
+        while True:
+            status = jobCollection.get(
+                projectId=projectId,
+                jobId=insertResponse['jobReference']['jobId']).execute()
+            currentStatus = status['status']['state']
+
+            if 'DONE' == currentStatus:
+                currentRow = 0
+                queryReply = jobCollection.getQueryResults(
+                    projectId=projectId,
+                    jobId=insertResponse['jobReference']['jobId'],
+                    startIndex=currentRow).execute()
+
+                while ('rows' in queryReply and
+                       currentRow < queryReply['totalRows']):
+                    printTableData(queryReply, currentRow)
+                    currentRow += len(queryReply['rows'])
+                    queryReply = jobCollection.getQueryResults(
                         projectId=projectId,
                         jobId=queryReply['jobReference']['jobId'],
                         startIndex=currentRow).execute()
+            else:
+                print 'Waiting for the query to complete...'
+                print 'Current status: ' + currentStatus
+                print time.ctime()
+                time.sleep(10)
 
-  except HttpError as err:
-    print 'Error in runAsyncTempTable:', pprint.pprint(err.resp)
+    except HttpError as err:
+        print 'Error in runAsyncTempTable:', pprint.pprint(err.resp)
 
-  except Exception as err:
-    print 'Undefined error' % err
-# [END asynchronous_call]
-
-# [START batched_call]
-def runBatchedCall():
-  try:
-    jobCollection = service.jobs()
-    queryString = 'SELECT corpus FROM publicdata:samples.shakespeare GROUP BY corpus;'
-    jobData = {
-      'configuration': {
-        'query': {
-          'query': queryString,
-          'priority': 'BATCH' # Set priority to BATCH
-        }
-      }
-    }
-
-    insertResponse = jobCollection.insert(projectId=projectId,
-                                         body=jobData).execute()
-
-    import time
-    while True:
-      status = jobCollection.get(projectId=projectId, jobId=insertResponse['jobReference']['jobId']).execute()
-      currentStatus = status['status']['state']
-
-      if 'DONE' == currentStatus:
-        currentRow = 0
-        queryReply = jobCollection.getQueryResults(
-                       projectId=projectId,
-                       jobId=insertResponse['jobReference']['jobId'],
-                       startIndex=currentRow).execute()
-
-        while(('rows' in queryReply) and currentRow < queryReply['totalRows']):
-          printTableData(queryReply, currentRow)
-          currentRow += len(queryReply['rows'])
-          queryReply = jobCollection.getQueryResults(
-                         projectId=projectId,
-                         jobId=queryReply['jobReference']['jobId'],
-                         startIndex=currentRow).execute()
-      else:
-        print 'Waiting for the query to complete...'
-        print 'Current status: ' + currentStatus
-        print time.ctime()
-        time.sleep(10)
-
-  except HttpError as err:
-    print 'Error in runAsyncTempTable:', pprint.pprint(err.resp)
-
-  except Exception as err:
-    print 'Undefined error: %s' % err
+    except Exception as err:
+        print 'Undefined error: %s' % err
 # [END batched_call]
