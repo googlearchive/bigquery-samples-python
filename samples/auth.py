60ec45ff66133fb8bc9c6b79303debf4360f8d17
elibixby <elibixby@localhost>
2015-02-18 14:51:01 -0800

    Fixed bad merge

--- samples/auth.py
+++ samples/auth.py
@@ -1,15 +1,9 @@
 """[START auth]"""
-import httplib2
 from oauth2client.client import GoogleCredentials
 from googleapiclient.discovery import build
-<<<<<<< HEAD
-=======
-
->>>>>>> ccccfe9b549a493001478b0fcd8c4df1cf123ec6
 
 def get_service():
     credentials = GoogleCredentials.get_application_default()
-    http = credentials.authorize(httplib2.Http())
-    return build('bigquery', 'v2', http=http)
+    return build('bigquery', 'v2', credentials=credentials)
 
 """[END auth]"""
