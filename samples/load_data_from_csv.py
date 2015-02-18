97e20b151672aaad50b1c25584b1de926b8a714a
Wes <readybreak@gmail.com>
2015-02-18 15:24:58 -0800

    lint fixes for load_data_from_csv.

--- samples/load_data_from_csv.py
+++ samples/load_data_from_csv.py
@@ -1,42 +1,42 @@
-#[START load_table_from_csv]
+# [START load_table_from_csv]
 def load_table(service, project_id, dataset_id, table_id, source_csv):
     job_collection = service.jobs()
     job_data = {
-            'projectId': project_id,
-            'configuration': {
-                    'load': {
-                            'sourceUris': [source_csv],
-                            'schema': {
-                                    'fields': [
-                                            {
-                                                    'name': 'Name',
-                                                    'type': 'STRING'
-                                                    },
-                                            {
-                                                    'name': 'Age',
-                                                    'type': 'INTEGER'
-                                                    },
-                                            {
-                                                    'name': 'Weight',
-                                                    'type': 'FLOAT'
-                                                    },
-                                            {
-                                                    'name': 'IsMagic',
-                                                    'type': 'BOOLEAN'
-                                                    }
-                                            ]
-                                    },
-                            'destinationTable': {
-                                    'projectId': project_id,
-                                    'datasetId': dataset_id,
-                                    'tableId': table_id
-                                    },
-                            }
-                    }
+        'projectId': project_id,
+        'configuration': {
+            'load': {
+                'sourceUris': [source_csv],
+                'schema': {
+                    'fields': [
+                        {
+                            'name': 'Name',
+                            'type': 'STRING'
+                        },
+                        {
+                            'name': 'Age',
+                            'type': 'INTEGER'
+                        },
+                        {
+                            'name': 'Weight',
+                            'type': 'FLOAT'
+                        },
+                        {
+                            'name': 'IsMagic',
+                            'type': 'BOOLEAN'
+                        }
+                    ]
+                },
+                'destinationTable': {
+                    'projectId': project_id,
+                    'datasetId': dataset_id,
+                    'tableId': table_id
+                },
             }
+        }
+    }
 
-    job_resource = jobCollection.insert(
-            projectId=project_id,
-            body=job_data).execute()
-#[END load_table_from_csv]
+    job_resource = job_collection.insert(
+        projectId=project_id,
+        body=job_data).execute()
+# [END load_table_from_csv]
     return job_resource
