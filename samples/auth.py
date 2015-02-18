3732bec598507411c0e212341853578309ad5286
Wes <readybreak@gmail.com>
2015-02-18 15:30:54 -0800

    lint fix for auth.

--- samples/auth.py
+++ samples/auth.py
@@ -2,6 +2,7 @@
 from oauth2client.client import GoogleCredentials
 from googleapiclient.discovery import build
 
+
 def get_service():
     credentials = GoogleCredentials.get_application_default()
     return build('bigquery', 'v2', credentials=credentials)
