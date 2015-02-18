669032d61b19273d97b92a91b9bc90858ad1403f
Wes <readybreak@gmail.com>
2015-02-18 13:59:16 -0800

    Make test/ into a package. Rename auth_test->test_auth and rewrite it as a unittest. Make tox run python's unittest discovery.

---
+++ test/test_auth.py
@@ -0,0 +1,15 @@
+"""Tests for auth."""
+import unittest
+from samples import auth
+
+class TestAuth(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_service(self):
+        service = auth.get_service()
+        self.assertIsNotNone(service)
+
+if __name__ == 'main':
+    unittest.main()
